8b8513edc280669e0038946cf564f02036abd229
KarlAru <kiri@karlaru.ee>
2022-11-30 11:12:07 +0200

    Class for not 200 responses

---
+++ src/main/java/com/karlaru/tcw/response/models/Unsucess.java
@@ -0,0 +1,21 @@
+package com.karlaru.tcw.response.models;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement(name = "errorResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Unsucess extends Throwable{
+    @XmlElement(name = "statusCode")
+    private String code;
+    @XmlElement(name = "error")
+    private String message;
+}
