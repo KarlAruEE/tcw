74cc5cd67fbea07bea84e476c97b3d164c5486d2
KarlAru <kiri@karlaru.ee>
2022-12-04 19:21:29 +0200

    Tests

--- src/main/java/com/karlaru/tcw/response/models/ErrorResponse.java
+++ src/main/java/com/karlaru/tcw/response/models/ErrorResponse.java
@@ -2,8 +2,9 @@ package com.karlaru.tcw.response.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
-
+@Data
 @AllArgsConstructor
 public class ErrorResponse {
     @JsonProperty("code")
